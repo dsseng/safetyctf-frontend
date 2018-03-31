d4b4ec694620d9a66b7ca8ecc73a215c9fec894a
Dmitriy Sharshakov aka. sh7dm <sh7dm@outlook.com>
2018-03-31 21:35:03 +0300

    Added Axios as $http

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+import axios from 'axios'
 import App from './App'
 import router from './router'
 
+Vue.use(Vuetify)
+Vue.prototype.$http = axios.create({
+  baseURL: 'localhost:3000'
+})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
