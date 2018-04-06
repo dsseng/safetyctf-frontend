5170f59c383cb90897bf93903ff5cab38a80a9bd
Dmitriy Sharshakov aka. sh7dm <sh7dm@outlook.com>
2018-04-06 08:05:42 +0300

    Added tasks support 📜📜📜

--- src/components/Game/Tasks.vue
+++ src/components/Game/Tasks.vue
@@ -1,32 +1,48 @@
 <template>
 <div>
   <v-container v-if="this.auth">
+    <v-alert type="error" icon="signal_cellular_connected_no_internet_4_bar" :value="err" transition="scale-transition">
+      It seems to be some connection problems
+    </v-alert>
     <v-layout row justify-space-around>
-      <div>asd</div>
-      <div>asd</div>
-      <div>asd</div>
-      <div>asd</div>
-      <div>asd</div>
-      <div>asd</div>
-      <div>asd</div>
+      <task v-for="task in tasks" :key="task.id" :task="task"></task>
     </v-layout>
   </v-container>
 </div>
 </template>
 
 <script>
+import Task from './Task'
+
 export default {
+  components: { Task },
   data () {
     return {
       auth: false,
-      admin: false
+      admin: false,
+      tasks: [],
+      err: false
     }
   },
-  created () {
+  async created () {
     setInterval(() => {
       this.auth = this.$getAuth()
       if (!this.auth) this.$router.push('/game/myaccount')
     }, 50)
+
+    try {
+      let result = await this.$http.get(this.$apiRoot + 'tasks/')
+
+      if (result.data.code === 200) {
+        this.tasks = result.data.tasks
+      } else {
+        console.error(result.data)
+        this.err = true
+      }
+    } catch (err) {
+      console.error(err)
+      this.err = true
+    }
   }
 }
 </script>
