c2615cf66ed71fc2bc48bc99826e940067ac650d
sh7dm <sh7dm@outlook.com>
2018-05-17 13:48:09 +0300

    🎉 Added 404 animation!

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -6,6 +6,10 @@
     class="blue--text"
     style="width: 100%; height: 100vh;"
   >
+    <div class="term">
+      <span class="typed"></span>
+    </div>
+
     <h1 class="blue--text mb-2 display-1 text-xs-center">🤬☹️Hmm... Page not found</h1>
     <v-btn
       class="blue lighten-2 mt-5"
@@ -27,7 +31,37 @@
 </template>
 
 <script>
-export default {
+import Typed from 'typed.js'
 
+export default {
+  async mounted () {
+    var ip = (await this.$http.get('//icanhazip.com/')).data
+    ip = ip.substring(0, ip.length - 1) // Remove \n
+
+    /* eslint-disable no-new */
+    new Typed('.typed', {
+      strings: ['wget https://safetyctf.com' + this.$route.path + '^1000\n`--' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + '--  https://safetyctf.com' + this.$route.path + '`^1000 \n`Resolving safetyctf.com`^750.^750.^750. ^1000 `' + ip + '` ^1000\n`Connecting to safetyctf.com|' + ip + '|:80`^750.^750.^750. `connected.`\n`HTTP request sent, awaiting response`^750.^750.^750. `404 Not Found`\n`' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + ' ERROR 404: Not Found.`'],
+      typeSpeed: 40,
+      backSpeed: 0,
+      loop: true
+    })
+  }
 }
 </script>
+
+<style scoped>
+.term {
+  background-color: #333;
+  color: white;
+  width: 30rem;
+  height: 10rem;
+}
+
+.typed {
+  white-space: pre;
+}
+.typed::before {
+  content: '$ ';
+  color: lightgreen;
+}
+</style>
