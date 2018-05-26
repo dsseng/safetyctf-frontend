16c292dd67a822516a9a96c14331ec5d22a44c8a
sh7dm <sh7dm@outlook.com>
2018-05-26 16:38:36 +0300

    🔁 Shortened user info request URL

--- src/components/Game/User.vue
+++ src/components/Game/User.vue
@@ -42,7 +42,7 @@ export default {
   props: [ 'username' ],
   async created () {
     try {
-      let result = await this.$http.get(this.$apiRoot + 'info/' + this.username + '/info')
+      let result = await this.$http.get(this.$apiRoot + 'info/' + this.username)
 
       if (result.data.code === 200) {
         if (!result.data.user) {
@@ -68,7 +68,7 @@ export default {
   watch: {
     async username () {
       try {
-        let result = await this.$http.get(this.$apiRoot + 'auth/' + this.username + '/info')
+        let result = await this.$http.get(this.$apiRoot + 'info/' + this.username)
 
         if (result.data.code === 200) {
           if (!result.data.user) {
